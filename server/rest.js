2c4ad2d05a2aaec2a7ef260be6fb6c8ea60f697a
Peter Menocal <peter@geekypixel.com>
2017-06-05 12:23:45 -0700

    Align operators

--- server/rest.js
+++ server/rest.js
@@ -1,14 +1,14 @@
 'use strict'
 
-var express = require('express')
-var app = express()
+var express    = require('express')
+var app        = express()
 var bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 
 app.get('/dog/:id', function (req, res, next) {
   res.send({
-    id: req.params.id,
+    id  : req.params.id,
     name: 'Dolly'
   })
 })
