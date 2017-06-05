9d977d63ff97087195e97440020573438293c143
Peter Menocal <peter@geekypixel.com>
2017-06-05 12:23:16 -0700

    Align operators

--- server/web.js
+++ server/web.js
@@ -1,16 +1,15 @@
 'use strict'
 
 var express = require('express')
-var routes = require('../client/routes')
-var toHtml = require('mithril-node-render')
-var m = require('mithril')
-
-var app = express()
+var routes  = require('../client/routes')
+var toHtml  = require('mithril-node-render')
+var m       = require('mithril')
+var app     = express()
 
 Object.keys(routes).map(function (route) {
-  const module = routes[route]
+  const module  = routes[route]
   const onmatch = module.onmatch || (() => module)
-  const render = module.render || (a => a)
+  const render  = module.render || (a => a)
   app.get(route, function (req, res, next) {
     res.type('html')
     Promise.resolve()
